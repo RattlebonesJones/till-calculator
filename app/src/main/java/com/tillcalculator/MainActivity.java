503cc1c7478d6227a737b24e059b10333fdb2ca5
RattlebonesJones <49927998+RattlebonesJones@users.noreply.github.com>
2025-04-29 18:59:42 -0400

    add subtotal fields for each currency type

--- app/src/main/java/com/tillcalculator/MainActivity.java
+++ app/src/main/java/com/tillcalculator/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
@@ -17,15 +18,32 @@ import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     BottomNavigationView bottomNavigationView;
+    BillsFragment firstFragment = new BillsFragment();
+    CoinsFragment secondFragment = new CoinsFragment();
+    RollsFragment thirdFragment = new RollsFragment();
+    BigDecimal miscBills = new BigDecimal(0).setScale(2, RoundingMode.HALF_EVEN);
+    BigDecimal tensSum = new BigDecimal(0).setScale(2, RoundingMode.HALF_EVEN);
+    BigDecimal fivesSum = new BigDecimal(0).setScale(2, RoundingMode.HALF_EVEN);
+    BigDecimal onesSum = new BigDecimal(0).setScale(2, RoundingMode.HALF_EVEN);
+    BigDecimal quartersSum = new BigDecimal(0).setScale(2, RoundingMode.HALF_EVEN);
+    BigDecimal dimesSum = new BigDecimal(0).setScale(2, RoundingMode.HALF_EVEN);
+    BigDecimal nickelsSum = new BigDecimal(0).setScale(2, RoundingMode.HALF_EVEN);
+    BigDecimal penniesSum = new BigDecimal(0).setScale(2, RoundingMode.HALF_EVEN);
+    BigDecimal quartersRollSum = new BigDecimal(0).setScale(2, RoundingMode.HALF_EVEN);
+    BigDecimal dimesRollSum = new BigDecimal(0).setScale(2, RoundingMode.HALF_EVEN);
+    BigDecimal nickelsRollSum = new BigDecimal(0).setScale(2, RoundingMode.HALF_EVEN);
+    BigDecimal penniesRollSum = new BigDecimal(0).setScale(2, RoundingMode.HALF_EVEN);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
@@ -33,26 +51,63 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
     }
 
-    BillsFragment firstFragment = new BillsFragment();
-    CoinsFragment secondFragment = new CoinsFragment();
-    RollsFragment thirdFragment = new RollsFragment();
+    private BigDecimal createBigDecimal(double n) {
+        return new BigDecimal(n).setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    private BigDecimal getField(int id, double scalar) {
+        String asString = ((EditText) findViewById(id)).getText().toString();
+        int asInt = 0;
+        try {
+            asInt = Integer.parseInt(asString);
+        } catch (NumberFormatException ignored) {
+        }
+        return createBigDecimal(asInt).multiply(createBigDecimal(scalar)).setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    private void setSumView(int id, BigDecimal val){
+        ((TextView) findViewById(id)).setText(val.toString());
+    }
 
     public void getSum(View view) {
-        double miscBills = Integer.parseInt(((EditText) findViewById(R.id.miscBills)).getText().toString());
-        double tensSum = 10 * Integer.parseInt(((EditText) findViewById(R.id.tens)).getText().toString());
-        double fivesSum = 5 *Integer.parseInt(((EditText) findViewById(R.id.fives)).getText().toString());
-        double onesSum = Integer.parseInt(((EditText) findViewById(R.id.ones)).getText().toString());
-        double quartersSum = 0;
-        double dimesSum = 0;
-        double nickelsSum = 0;
-        double penniesSum = 0;
-        double quartersRollSum = 0;
-        double dimesRollSum = 0;
-        double nickelsRollSum = 0;
-        double penniesRollSum = 0;
-
-        double sum = miscBills + tensSum + fivesSum + onesSum + quartersSum + dimesSum + nickelsSum + penniesSum + quartersRollSum + dimesRollSum + nickelsRollSum + penniesRollSum;
-        ((TextView)findViewById(R.id.totalView)).setText(Double.toString(sum));
+        switch (bottomNavigationView.getSelectedItemId()) {
+            case R.id.billsFragment:
+                miscBills = getField(R.id.miscBills, 1);
+                setSumView(R.id.miscBillsTotal, miscBills);
+                tensSum = getField(R.id.tens, 10);
+                setSumView(R.id.tensTotal, tensSum);
+                fivesSum = getField(R.id.fives, 5);
+                setSumView(R.id.fivesTotal, fivesSum);
+                onesSum = getField(R.id.ones, 1);
+                setSumView(R.id.onesTotal, onesSum);
+                break;
+            case R.id.coinsFragment:
+                quartersSum = getField(R.id.quarters, 0.25);
+                setSumView(R.id.quartersTotal, quartersSum);
+                dimesSum = getField(R.id.dimes, 0.1);
+                setSumView(R.id.dimesTotal, dimesSum);
+                nickelsSum = getField(R.id.nickels, 0.05);
+                setSumView(R.id.nickelsTotal, nickelsSum);
+                penniesSum = getField(R.id.pennies, 0.01);
+                setSumView(R.id.penniesTotal, penniesSum);
+                break;
+            case R.id.rollsFragment:
+                quartersRollSum = getField(R.id.quartersRolls, 10);
+                setSumView(R.id.quartersRollsTotal, quartersRollSum);
+                dimesRollSum = getField(R.id.dimesRolls, 5);
+                setSumView(R.id.dimesRollsTotal, dimesRollSum);
+                nickelsRollSum = getField(R.id.nicklesRolls, 2);
+                setSumView(R.id.nickelsRollsTotal, nickelsRollSum);
+                penniesRollSum = getField(R.id.penniesRolls, 0.5);
+                setSumView(R.id.penniesRollsTotal, penniesRollSum);
+                break;
+        }
+        BigDecimal[] values = {miscBills, tensSum, fivesSum, onesSum, quartersSum, dimesSum, nickelsSum, penniesSum, quartersRollSum, dimesRollSum, nickelsRollSum, penniesRollSum};
+        BigDecimal sum = createBigDecimal(0);
+        for (BigDecimal i : values) {
+            sum = sum.add(i);
+        }
+        setSumView(R.id.totalView, sum);
     }
 
     @Override
